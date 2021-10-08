7aad9458bc35b245230821efdf39cda54e39e223
Matthew Waller <50606537+MattWaller@users.noreply.github.com>
2021-10-07 18:57:21 -0700

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-apk add --no-cache curl openvpn tinyproxy bash
+apk add --no-cache curl; 
+apk add --no-cache openvpn ;
+apk add --no-cache tinyproxy;
+
+
+mkdir -p /dev/net \
+mknod /dev/net/tun c 10 200 \
+chmod 0666 /dev/net/tun
